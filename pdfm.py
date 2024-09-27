58add91ba724a4799be98d471550a9ad167e9861
Ishika Yadav <167458433+Yadav-Ishika@users.noreply.github.com>
2024-09-27 23:17:23 +0530

    pdfm.py

--- pdfm.py
+++ pdfm.py
@@ -97,8 +97,8 @@ if 'merge_pdf_paths' not in st.session_state:
 if 'split_pdf_paths' not in st.session_state:
     st.session_state.split_paths = None
 
-if 'rearrang_pdf_paths' not in st.session_state:
-    st.session_state.rearrang_paths = None
+if 'rearrange_pdf_paths' not in st.session_state:
+    st.session_state.rearrange_paths = None
 
 if 'watermark_pdf_paths' not in st.session_state:
     st.session_state.watermark_paths = None
@@ -135,7 +135,7 @@ if st.sidebar.button("Split PDF"):
     st.session_state.conversion_type = "spdf"
     placeholder.empty()
 
-if st.sidebar.button("Rearrang PDF Pages"):
+if st.sidebar.button("Rearrange PDF Pages"):
     st.session_state.conversion_type = "rapdf"
     placeholder.empty()
 
@@ -1150,7 +1150,7 @@ if st.session_state.conversion_type == "rtpdf":
 
 st.sidebar.markdown("""
     <div style='text-align: center; display: flex; flex-direction: column; gap: -100px;'>
-        <p style='font-size: 1vw;'>Developed by Rayyan Ashraf</p>
+        <p style='font-size: 1vw;'>Developed by Ishika Yadav</p>
         <p style='font-size: 1vw;'>Copyright @ 2024</p>
         <p style='font-size: 1vw;'>Developed in Python 3.12.5</p>
     </div>
